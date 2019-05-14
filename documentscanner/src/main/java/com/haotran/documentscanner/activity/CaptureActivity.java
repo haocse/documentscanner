435c7d56d02553aceb75752730ee9aaeba1c99b6
Hao Tran <HaoCSE@users.noreply.github.com>
2019-05-14 19:24:54 +0700

    ...

--- documentscanner/src/main/java/com/haotran/documentscanner/activity/CaptureActivity.java
+++ documentscanner/src/main/java/com/haotran/documentscanner/activity/CaptureActivity.java
@@ -1,10 +1,13 @@
 package com.haotran.documentscanner.activity;
 
+import android.os.Build;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.LinearLayout;
 
 import com.haotran.documentscanner.R;
@@ -18,5 +21,18 @@ public class CaptureActivity extends AppCompatActivity {
 
         LinearLayout toobar = (LinearLayout) findViewById(R.id.toolbar);
         toobar.setBackgroundColor(ContextCompat.getColor(this, R.color.colorPrimaryDark));
+        setStatusBarColor(R.color.colorPrimaryDark);
+
     }
+
+
+    public void setStatusBarColor(int color) {
+        Window window = getWindow();
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            window.setStatusBarColor(ContextCompat.getColor(this, color));
+        }
+    }
+
 }
