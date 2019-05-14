d9014ae838a46fe11de9b74dee6b1c65d882a71d
Hao Tran <HaoCSE@users.noreply.github.com>
2019-05-14 18:31:41 +0700

    ..

--- documentscanner/src/main/java/com/haotran/documentscanner/activity/CaptureActivity.java
+++ documentscanner/src/main/java/com/haotran/documentscanner/activity/CaptureActivity.java
@@ -1,7 +1,11 @@
 package com.haotran.documentscanner.activity;
 
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.LinearLayout;
 
 import com.haotran.documentscanner.R;
 
@@ -11,5 +15,8 @@ public class CaptureActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_capture);
+
+        LinearLayout toobar = (LinearLayout) findViewById(R.id.toolbar);
+        toobar.setBackgroundColor(ContextCompat.getColor(this, R.color.colorPrimaryDark));
     }
 }
